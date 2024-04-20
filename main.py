b2a39c9b641f3f7a5c491bd7db459c229d0b5c0f
Nikita Guzarskii <mr.marser007@gmail.com>
2024-04-20 14:52:08 +0300

    main

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+from pathlib import Path
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMenuBar, QStatusBar, QFileDialog
+from PyQt6.QtGui import QAction
+
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("scenarioEditor")
+        self.setGeometry(0,0,1920,1080)
+        self.showMaximized()
+
+    def barMenu(self):
+        menu = self.menuBar()
+        fileMenu = menu.addMenu("&File")
+
+
+        openAction = QAction("&Open", self)
+        fileMenu.addAction(openAction)
+        
+        saveAction = QAction("&Save", self)
+        fileMenu.addAction(saveAction)
+
+        saveAsAction = QAction("&Save As...", self)
+        fileMenu.addAction(saveAsAction)
+        
+        fileMenu.addSeparator()
+
+        exitAction = QAction("&Exit", self)
+        fileMenu.addAction(exitAction)
+
+
+
+
+        exitAction.triggered.connect(self.close)
+
+
+        openAction.setShortcut("Ctrl+O")
+        saveAction.setShortcut("Ctrl+S")
+        saveAsAction.setShortcut("Ctrl+Shift+S")
+        exitAction.setShortcut("Ctrl+Q")
+
+
+    def openFile(self):
+        pass
+      
+# Инициализируем и запускаем приложение
+app = QApplication(sys.argv)
+window = MainWindow()
+window.barMenu()
+window.show()
+
+app.setStyleSheet(Path("style.css").read_text())
+sys.exit(app.exec())
